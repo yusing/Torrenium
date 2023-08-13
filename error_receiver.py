f5a03750f54bb73379bd3bf52f33d0259603af40
yusing <yusing.wys@gmail.com>
2023-08-13 18:36:54 +0800

    debug ios

--- error_receiver.py
+++ error_receiver.py
@@ -1,10 +1,22 @@
+import json
 import websockets
 import asyncio
 from websockets.server import serve
 
 async def error_receiver(websocket):
-    async for msg in websocket:
-        print(msg)
+    try:
+        async for msg in websocket:
+            msg = json.loads(msg)
+            print('------------------')
+            print('Error received:')
+            print('Error:', msg['error'])
+            print('Message:', msg['message'])
+            print('Stacktrace:', msg['stackTrace'])
+            print('------------------')
+    except websockets.exceptions.ConnectionClosed:
+        pass
+    except Exception as e:
+        print(e)
 
 async def main():
     async with serve(error_receiver, "localhost", 5999):
