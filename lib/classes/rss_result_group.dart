655c28dda7b501dfbc9f9076d97eade938209a22
Yuzerion <yusing.wys@gmail.com>
2023-08-17 02:35:21 +0000

    roadmap wip

--- lib/classes/rss_result_group.dart
+++ lib/classes/rss_result_group.dart
@@ -1,10 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/widgets.dart';
 import 'package:macos_ui/macos_ui.dart';
 
 import '../main.dart' show kIsDesktop;
-import '../services/torrent.dart';
 import '../widgets/rss_result_dialog.dart';
 import 'item.dart';
 
@@ -12,14 +9,15 @@ class RssResultGroup {
   final MapEntry<String, List<Item>> result;
 
   RssResultGroup(this.result);
-  
+
   List<Item> get items => result.value;
   String get title => result.key;
 
-    Future<void> showDialog(BuildContext context) async {
+  Future<void> showDialog(BuildContext context) async {
     if (kIsDesktop) {
       showMacosSheet(
-          context: context, builder: (context) => RssResultDialog(context, this));
+          context: context,
+          builder: (context) => RssResultDialog(context, this));
     } else {
       Navigator.push(
           context,
@@ -32,8 +30,8 @@ class RssResultGroup {
                       softWrap: true,
                     ),
                   ),
-                  child: SafeArea(child: RssResultDialog.content(context, this)))));
+                  child: SafeArea(
+                      child: RssResultDialog.content(context, this)))));
     }
   }
-
 }
